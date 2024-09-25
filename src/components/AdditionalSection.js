9b948fe6312747187aa1fbcd59a66194416120b3
rayblaxe <raihan21ti@mahasiswa.pcr.ac.id>
2024-09-25 14:34:10 +0700

    Fix Home
    Former-commit-id: ffba74dc1021c9a2cfaa67d81042d7fd9ebe63ff
    Former-commit-id: 735711698c0651b64fd1723bf26ac67f9a2897e5

--- src/components/AdditionalSection.js
+++ src/components/AdditionalSection.js
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image";
 
 const links = [
   {
-    icon: "/frontend/src/images/heartlogo.png",
+    icon: "../images/heartlogo.png",
     text: "Pendaftaran Sertifikat Halal",
     href: "https://ptsp.halal.go.id",
   },
@@ -33,21 +33,68 @@ const links = [
 const AdditionalSection = () => {
   return (
     <section className="my-10 p-6 text-center">
-      <div className="container grid grid-flow-row-dense gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-        {links.map((link, index) => (
-          <Link
-            to={link.href}
-            key={index}
-            className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
-          >
-            <StaticImage
-              src={"/frontend/src/images/heartlogo.png"}
-              alt={link.text}
-              className="mx-auto mb-4 h-16"
-            />
-            <p className="text-sm font-semibold text-gray-700">{link.text}</p>
-          </Link>
-        ))}
+      <div className="container grid grid-flow-row-dense gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+        <Link
+          to="/link21"
+          className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
+        >
+          <StaticImage
+            src={"../images/heartlogo.png"}
+            alt="Pendaftaran Sertifikasi Halal"
+            className="mx-auto mb-4 h-24 w-16"
+          />
+          <p className="text-md font-semibold text-gray-700">
+            Pendaftaran Sertifikasi Halal
+          </p>
+        </Link>
+
+        <Link
+          to="/link2"
+          className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
+        >
+          <StaticImage
+            src={"../images/laporlogo.png"}
+            alt="Lapor"
+            className="mx-auto mt-4 mb-4 h-20 w-full"
+          />
+          <p className="text-md font-bold text-gray-700">Lapor</p>
+        </Link>
+
+        <Link
+          to="/link3"
+          className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
+        >
+          <StaticImage
+            src={"halallogo.png"}
+            alt="Kuliner Halal"
+            className="mx-auto mb-4 h-24 w-24"
+          />
+          <p className="text-md font-bold text-gray-700">Kuliner Halal</p>
+        </Link>
+
+        <Link
+          to="/link4"
+          className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
+        >
+          <StaticImage
+            src={"../images/searchlogo.png"}
+            alt="Traceability Halal"
+            className="mx-auto mb-4 h-24 w-24"
+          />
+          <p className="text-md font-bold text-gray-700">Traceability Halal</p>
+        </Link>
+
+        <Link
+          to="/link5"
+          className="block rounded-lg border bg-white p-3 shadow-md transition hover:shadow-lg"
+        >
+          <StaticImage
+            src={"../images/searchlogo.png"}
+            alt="Traceability Halal"
+            className="mx-auto mb-4 h-24 w-14"
+          />
+          <p className="text-md font-bold  text-gray-700">Traceability Halal</p>
+        </Link>
       </div>
     </section>
   );
